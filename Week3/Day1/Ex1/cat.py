2e03ff0eda3771a15283741c75c941b577e0433d
lukerosssa2022 <lukerosssa@gmail.com>
2023-04-03 09:20:58 +0300

    cat

---
+++ Week3/Day1/Ex1/cat.py
@@ -0,0 +1,18 @@
+class Cat:
+    def __init__(self, cat_name, cat_age):
+        self.name = cat_name
+        self.age = cat_age
+
+cat1 = Cat("Tom", 2)
+cat2 = Cat("Garfield", 5)
+cat3 = Cat("Whiskers", 1)
+
+def find_oldest_cat(*cats):
+    oldest_cat = None
+    for cat in cats:
+        if not oldest_cat or cat.age > oldest_cat.age:
+            oldest_cat = cat
+    return oldest_cat
+
+oldest_cat = find_oldest_cat(cat1, cat2, cat3)
+print(f"The oldest cat is {oldest_cat.name}, and is {oldest_cat.age} years old.")
